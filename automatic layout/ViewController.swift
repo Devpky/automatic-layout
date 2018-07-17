533ae619c764738e071e4db49ab6aa8d8c5381e1
pavan yadav <pavan.yadav@invetechsolutions.com>
2018-07-17 12:28:08 +0530

    Initial Commit

---
+++ automatic layout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  automatic layout
+//
+//  Created by Pawan Yadav on 17/07/18.
+//  Copyright © 2018 invetech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
